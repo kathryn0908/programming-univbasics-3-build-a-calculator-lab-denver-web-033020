44fc791a047115f1c76adb97dbc404626459d54e
Kathryn Clark <kathrynlael@gmail.com>
2020-03-04 17:02:41 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,20 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  sum
 end
 
 def subtraction(num1, num2)
-
+difference = num2 - num1
+difference
 end
 
 def division(num1, num2)
-
+quotient = num1/num2
+quotient
 end
 
 def multiplication(num1, num2)
-
+product
 end
 
 def modulo(num1, num2)
